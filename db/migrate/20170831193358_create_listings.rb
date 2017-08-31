4630e67e0fa3f94ae9eebe9a2a91e246af26e78e
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-08-31 14:18:04 -0700

    setup schema for both migration models

--- db/migrate/20170831193358_create_listings.rb
+++ db/migrate/20170831193358_create_listings.rb
@@ -1,6 +1,14 @@
 class CreateListings < ActiveRecord::Migration[5.1]
   def change
     create_table :listings do |t|
+      t.string :address
+      t.string :title
+      t.string :rent
+      t.string :available_date
+      t.string :description
+      t.string :contact_phone
+      t.string :contact_email
+      t.string :city
 
       t.timestamps
     end
