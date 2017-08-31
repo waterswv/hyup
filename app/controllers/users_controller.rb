b1b18840a1d2dd793cccb9f700720eb12214f434
stacysn <stacysuen5@gmail.com>
2017-08-31 15:12:48 -0700

    created signup form in new.html.erb and built new and create methods in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def new
+    @new_user = User.new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:name, :phone, :email, :password_digest)
+    user = User.new(user_params)
+
+    if user.save
+      redirect_to "/users/new"
+    end
+
+  end
+
 end
