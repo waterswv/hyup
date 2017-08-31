ac889738feeefd1a0185cf99167b98b03d0bd1fd
CalJersey <crippe@gmail.com>
2017-08-31 13:50:16 -0700

    added routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "listing#index"
+  get "/listings", to: "listings#index", as:"listings"
+  get "/listings/new", to: "listings#new", as:"new_listing"
+  post "/listings", to: "listings#create"
+  get "/listings/:id", to:"listings#show", as:"listing"
+  get "/listings/:id/edit", to:"listings#edit", as:"edit_listing"
+  post "/listings/:id", to:"listing#update"
+  delete "/listings/:id", to:"listings#destroy"
+
+  #signup form
+  get "/users/new", to: "users#new", as:"new_user"
+  post "/users", to: "users#create"
+  #get "/users/:id", to:"users#show", as:"user"
+  get "/users/:id/edit", to:"users#edit", as:"edit_user"
+  #post "/users/:id", to:"user#edit"
+  #delete "/users/:id", to:"users#destroy"
+
+  post "/login", to: "session#login"
+  get "/logout", to:"session#destroy"
+
 end
