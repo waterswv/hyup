a4e5a8ca4abe88b4c37aa70fccbab0bd134e89f1
stacysn <stacysuen5@gmail.com>
2017-09-01 10:52:06 -0700

    created sessions in sessions controller and built confirm method in user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,31 @@ class SessionsController < ApplicationController
     end
 
     def create
-      user = User.find_by_email(params[:email])
-      # If the user exists AND the password entered is correct.
-      if user && user.authenticate(params[:password])
-        # Save the user id inside the browser cookie. This is how we keep the user
-        # logged in when they navigate around our website.
-        session[:user_id] = user.id
-        redirect_to '/users/new'
+      user_params = params.require(:user).permit(:email, :password)
+      @user = User.confirm(user_params)
+
+      if @user
+         session[:user_id] = @user.id
+         p session[:user_id]
+         redirect_to '/users/new'
       else
-        flash.alert = "We're not failures"
-        redirect_to '/users/new'
+        redirect_to '/login'
       end
     end
+
+    #
+    #   user = User.find_by_email(params[:email])
+    #   # If the user exists AND the password entered is correct.
+    #   if user && user.authenticate(params[:password])
+    #     # Save the user id inside the browser cookie. This is how we keep the user
+    #     # logged in when they navigate around our website.
+    #     session[:user_id] = user.id
+    #     redirect_to '/users/new'
+    #   else
+    #     # flash.alert = "We're not failures"
+    #     redirect_to '/login'
+    #   end
+    # end
 
     def destroy
       session[:user_id] = nil
