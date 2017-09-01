e0f5c94c9f795f6323afca26f6acba5e5a664fbe
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-09-01 14:09:22 -0700

    imported paperclip gem, updated Listing model, generated migration, executed migration for image addition to Listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,4 @@
 class Listing < ApplicationRecord
+  has_attached_file :image, styles: { large: "600x600>", medium: "400x400>", thumb: "100x100#"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
