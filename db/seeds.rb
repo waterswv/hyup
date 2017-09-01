7c260aa6a1ee9ea2024cc359970a3da50f3a12e1
ccpinotti <ccpinotti@gmail.com>
2017-08-31 17:40:38 -0700

    completed seed data and index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
+
+10.times do
+  Listing.create(
+    address: FFaker::AddressUS.street_address + ',' + FFaker::AddressUS.city + ',' + FFaker::AddressUS.state + ',' + FFaker::AddressUS.zip_code,
+    title: FFaker::DizzleIpsum.words,
+    rent: FFaker::Address.building_number,
+    available_date:  Date::MONTHNAMES[1+rand(11)],
+    description: FFaker::DizzleIpsum.paragraph,
+    contact_phone: FFaker::PhoneNumber.phone_number,
+    contact_email: FFaker::Internet.email,
+    city: 'San Francisco'
+  )
+end
