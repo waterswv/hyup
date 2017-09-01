3837b61b3ae238d29dcdb90937f49d07cc58e145
stacysn <stacysuen5@gmail.com>
2017-08-31 17:16:52 -0700

    debugging session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :phone, :email, :password_digest)
     user = User.new(user_params)
 
     if user.save
+      session[:user_id] = user.id
       redirect_to "/users/new"
     end
 
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :phone, :email, :password)
+  end
 end
