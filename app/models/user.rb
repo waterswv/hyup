43dfe9c04f7cae4fef58ec8442b96a543086740a
stacysn <stacysuen5@gmail.com>
2017-09-05 11:56:55 -0700

    added uniqueness to email sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   has_secure_password
 
   include Gravtastic
-  gravtastic 
+  gravtastic
 
 #validations
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+  validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
   validates :password_digest, presence: true
   validates :phone, presence: true, numericality: true, length: {minimum: 10, maximum: 15}
 
