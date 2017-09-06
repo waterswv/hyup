9357c7aafc96444c04f0e3c1a7f8152763eaf9ae
stacysn <stacysuen5@gmail.com>
2017-09-06 13:59:28 -0700

    migration file

---
+++ db/migrate/20170906180141_add_user_id_to_listings.rb
@@ -0,0 +1,7 @@
+class AddUserIdToListings < ActiveRecord::Migration[5.1]
+  def change
+    add_column :listings, :user_id, :bigint
+    add_foreign_key :listings, :users, column: :user_id, primary_key: :id
+  end
+end
+  
